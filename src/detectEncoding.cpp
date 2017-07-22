39dd05d7c8dd404555ce943dd8a2a08824c8164c
Artem Klevtsov <a.a.klevtsov@gmail.com>
2017-07-22 14:19:02 +0700

    Improve detectEncoding to handle NAs

--- src/detectEncoding.cpp
+++ src/detectEncoding.cpp
@@ -1,5 +1,4 @@
 #include <Rcpp.h>
-#include <string.h>
 #include "uchardet.h"
 
 using namespace Rcpp;
@@ -14,19 +13,26 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 CharacterVector detectEncoding(const CharacterVector& str) {
     R_xlen_t n = str.size();
-    
+    CharacterVector res(n);
     uchardet_t handle = uchardet_new();
-    
-    CharacterVector res = no_init(n);
-    
+
     for (R_xlen_t i = 0; i < n; ++i) {
-        uchardet_handle_data(handle, str[i], strlen(str[i]));
+        // Skip detecting for the NA
+        if (CharacterVector::is_na(str[i])) {
+            res[i] = NA_STRING;
+            continue;
+        }
+
+        int retval = uchardet_handle_data(handle, str[i], strlen(str[i]));
         uchardet_data_end(handle);
-        res[i]  = uchardet_get_charset(handle);
+        if (retval == 0)
+            res[i] = uchardet_get_charset(handle);
+        else
+            res[i] = NA_STRING;
         uchardet_reset(handle);
     }
-    
+
     uchardet_delete(handle);
-    
+
     return res;
 }

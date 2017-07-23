eb6936d780834a155657d35429b7cebf296ae80b
Artem Klevtsov <a.a.klevtsov@gmail.com>
2017-07-23 14:04:58 +0700

    Some fixes for the CRAN check

--- tests/testthat/test-Ruchardet.R
+++ tests/testthat/test-Ruchardet.R
@@ -1,13 +1,15 @@
 context("test Rucharet")
 
-test_that("test detectEncoding", {
-    expect_equal(detectFileEncoding(file.path("files", "big5.txt"), n =-1), "BIG5")
-    expect_equal(detectFileEncoding(file.path("files", "gb18030.txt"), n =-1), "GB18030")
-    expect_equal(detectFileEncoding(file.path("files", "shift_jis.txt"), n =-1), "SHIFT_JIS")
-    expect_equal(detectFileEncoding(file.path("files", "utf8.txt"), n = -1), "UTF-8")
-    expect_equal(detectFileEncoding(file.path("files", "euc_kr.txt"), n =-1), "EUC-KR")
+path <- system.file("test-files", package = "Ruchardet")
 
-    fo <- file(file.path("files", "shift_jis.txt"), 'r')
+test_that("test detectEncoding", {
+    expect_equal(detectFileEncoding(file.path(path, "big5.txt"), n =-1), "BIG5")
+    expect_equal(detectFileEncoding(file.path(path, "gb18030.txt"), n =-1), "GB18030")
+    expect_equal(detectFileEncoding(file.path(path, "shift_jis.txt"), n =-1), "SHIFT_JIS")
+    expect_equal(detectFileEncoding(file.path(path, "utf8.txt"), n = -1), "UTF-8")
+    expect_equal(detectFileEncoding(file.path(path, "euc_kr.txt"), n =-1), "EUC-KR")
+
+    fo <- file(file.path(path, "shift_jis.txt"), 'r')
     expect_equal(detectFileEncoding(fo), "SHIFT_JIS")
     close(fo)
 })
